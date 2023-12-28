4f2559853754172c69d6ed927f7d36721793f55e
Muhammad Bilal <bilalqureshi.epic@gmail.com>
2023-12-28 14:57:07 +0000

    Write ES7 snippets

---
+++ my-app/src/components/ES7Snippents.js
@@ -0,0 +1,55 @@
+
+// ---------- 1. rfce - function component -------------------
+// import React from 'react'
+
+// function ES7Snippents() {
+//   return (
+//     <div>ES7Snippents</div>
+//   )
+// }
+
+// export default ES7Snippents
+
+
+// ---------- 2. rafce - function component, export component as aerrow function -------------------
+// import React from 'react'
+
+// const ES7Snippents = () => {
+//   return (
+//     <div>ES7Snippents</div>
+//   )
+// }
+
+// export default ES7Snippents
+
+
+// ---------- 3. rce - class component -------------------
+// import React, { Component } from 'react'
+
+// export class ES7Snippents extends Component {
+//   render() {
+//     return (
+//       <div>ES7Snippents</div>
+//     )
+//   }
+// }
+
+// export default ES7Snippents
+
+
+// ---------- 4. rconst - create costructor for class component -------------------
+// constructor(props) {
+//   super(props)
+
+//   this.state = {
+//      first
+//   }
+// }
+
+
+// ---------- 5. clg - console.log(first) --------------------------------
+// console.log(first)
+
+
+// ----------- 6. clo to log an object along with its name --------------------------------------------
+console.log('object', object)
